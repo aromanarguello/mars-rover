5da1aa4373dd5d99222b3e4e4de7c57f86b2dda6
Alejandro Roman <aromanarguello@gmail.com>
2017-09-05 09:40:59 -0600

    erased few lines

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,70 +2,71 @@
 // ======================
 var rover = {
   direction: "N",
-  x: 0,
-  y: 0
+  coordinates: [0,0],
+  travelLog: []
 }
 
 // ======================
 function turnLeft(rover){
   console.log("turnLeft was called!");
+  switch(rover.direction) {
+    case "N":
+    rover.direction = "W"
+    break;
+    case "W":
+    rover.direction = "S"
+    break;
+    case "S":
+    rover.direction = "E"
+    break;
+    case "E":
+    rover.direction = "N"
+    break;
+  }
+  console.log("You are facing: " + rover.direction)
 }
 
 function turnRight(rover){
   console.log("turnRight was called!");
+  switch(rover.direction) {
+    case "N":
+    rover.direction = "E"
+    break;
+    case "E":
+    rover.direction = "S"
+    break;
+    case "S":
+    rover.direction = "W"
+    break;
+    case "W":
+    rover.direction = "N"
+    break;
+  }
+  console.log("You are facing: " + rover.direction)
 }
 
 function moveForward(rover){
   console.log("moveForward was called")
+
+  if (rover.direction === "N"){
+  rover.coordinates[1] = rover.coordinates[1] - 1;
+  rover.coordinates[0] = rover.coordinates[0]
 }
+  if (rover.direction === "S") {
+    rover.coordinates[1] = rover.coordinates[1] + 1;
+    rover.coordinates[0] = rover.coordinates[0];
+  }
 
+  if (rover.direction === "E") {
+    rover.coordinates[0] = rover.coordinates[0] + 1;
+    rover.coordinates[1] = rover.coordinates[1]
+  }
 
-
-//counter clockwise turning
-console.log(rover) // this log is just to keep trakc of the object
-
-switch(rover.direction){
-  case "N":
-  turnLeft();
-  rover.direction = "W";
-
-
-console.log(rover)
-
-  case "W":
-  turnLeft(rover);
-  rover.direction = "S";
-
-
-
-console.log(rover)
-
-  case "S":
-  turnLeft(rover);
-  rover.direction = "E"
-
-
-console.log(rover)
-
-  case "E":
-  turnLeft(rover);
-  rover.direction = "N"
-
-
-  console.log(rover)
+  if (rover.direction === "W") {
+    rover.coordinates[0] = rover.coordinates[0] - 1;
+    rover.coordinates[1] = rover.coordinates[1];
+  }
+console.log("You are facing: " + rover.direction)
+console.log("You are at coordinates: " + "X:" + rover.coordinates[0] + " and " + "Y:" + rover.coordinates[1])
+  }
 }
-
-
-
-
-//
-// if it changes direction then we can do an if condition for maybe another swithc statement
-//
-//
-//
-// if (rover.direction === "W" && moveForward(rover)){
-//   rover.x = rover.x - 1 ;
-// }
-//
-// moveForward(rover);
-// console.log(rover.x);
