bc22a38e53007f9bfa9b39068a49580409b6776a
Alejandro Roman <aromanarguello@gmail.com>
2017-09-06 16:20:36 -0600

    tracking iteration ideas

--- javascripts/app.js
+++ javascripts/app.js
@@ -67,15 +67,17 @@ function moveForward(rover){
     rover.coordinates[0] = rover.coordinates[0] - 1;
     rover.coordinates[1] = rover.coordinates[1];
   }
+
+
   console.log("You are facing: " + rover.direction)
   console.log("You are at coordinates: " + "X:" + rover.coordinates[0] + " and " + "Y:" + rover.coordinates[1])
   }
 
 function commands(lists) {
-  
+
   var str = lists
-  var str = str.split(""); // turned the results into an array
-  console.log(str);
+  var str = str.split(""); // turned the results into array behavior for iteration loop
+
 
   for (var i = 0; i < str.length; i++) {
 
@@ -88,9 +90,13 @@ function commands(lists) {
 
       if (str[i] === "f"){
         moveForward(rover);
+        coordinates = rover.travelLog += rover.coordinates;
+        rover.travelLog = coordinates;
+
+
+
       }
+
   }
-  for(i; i < str.length; i++) {
-    console.log("You have travelled: " + rover.travelLog)
-  }
+console.log(rover.travelLog);
 }
